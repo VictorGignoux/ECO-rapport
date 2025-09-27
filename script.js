0912ec66d03c1f0a5cd6a0dea74c497849c33e90
VictorGignoux <victor.gignoux@etudiant.univ-lr.fr>
2025-09-27 20:24:11 +0200

    -

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+});
+
+function selectRadar(index) {
+    const img = document.getElementById('tp1-radar-img');
+    img.setAttribute('src', `./img/radars/radar${index}.png`);
+
+    const current = document.querySelector('.radar-btn-current');
+    current.classList.remove('radar-btn-current');
+
+    const new_current = document.getElementById(`radar-btn-${index}`);
+    new_current.classList.add('radar-btn-current');
+}
